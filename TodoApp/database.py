1c03b46fd6724d5269eab7a536702689ec9b6a0a
HirajDey <deyhiraj@gmail.com>
2026-04-21 17:46:44 +0530

    connected FastAPI with production database - postgresql

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -3,10 +3,14 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from pathlib import Path
 
-_DB_PATH = (Path(__file__).resolve().parent / "todosapp.db").as_posix()
-SQLALCHEMY_DATABASE_URL = f"sqlite:///{_DB_PATH}"
+# _DB_PATH = (Path(__file__).resolve().parent / "todosapp.db").as_posix()
+# SQLALCHEMY_DATABASE_URL = f"sqlite:///{_DB_PATH}"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:123456@localhost:5432/TodoApplicationDatabase'
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
